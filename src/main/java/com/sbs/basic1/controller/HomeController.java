24cfad3ed84c1e3bffbf512ae8af746e80a1a72b
hyunseok93 <nowrock59@naver.com>
2025-05-17 15:17:42 +0900

    쿼리스트링 이용한 URL 파라미터 받아오기

--- src/main/java/com/sbs/basic1/controller/HomeController.java
+++ src/main/java/com/sbs/basic1/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.sbs.basic1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -19,24 +20,27 @@ public class HomeController {
   public String showHome(){
     return "어서오세요";
   }
+
   @GetMapping("/home/main2")
-  // 개발자가 /home/main 이라는 요청을 보내면
-  // 아래 메서드를 수행해줘
-  // -> 결과 : "어서오세요" 스트링 반환
   @ResponseBody
-  // 아래 메서드를 실행 후 리턴값을 응답으로 삼아
-  // body에 출력
   public String showHome2(){
     return "환영합니다";
   }
+
   @GetMapping("/home/main3")
-  // 개발자가 /home/main 이라는 요청을 보내면
-  // 아래 메서드를 수행해줘
-  // -> 결과 : "어서오세요" 스트링 반환
   @ResponseBody
-  // 아래 메서드를 실행 후 리턴값을 응답으로 삼아
-  // body에 출력
   public String showHome3(){
-    return "스프링부트는 획기적이다";
+
+    return "스프링부트는 획기적이다234";
+  }
+
+  @GetMapping("/home/plus")
+  @ResponseBody
+  public int showPlus(int a, @RequestParam(defaultValue = "0") int b){
+    // @RequestParam(defaultValue = "0")
+    // 쿼리스트링을 넣을 때 파라미터 인식가능하게끔 하는 방법
+    // 1) java compiler -> -parameters
+    // 2) 파라미터를 생략했을 때 기본값으로 인식하게끔 하는 방법 -> @RequestParam(defaultValue="0")
+    return a+b ;
   }
 }
